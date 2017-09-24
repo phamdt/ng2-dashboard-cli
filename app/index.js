5d7bd670e1b0cde7db37c2eeb974db44f270297d
Dinesh Dabhi <dinesh.dabhi@rapidops.com>
2017-09-25 02:21:47 +0530

    First Commit for the yo mean generator.

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+	// The name `constructor` is important here
+	constructor(args, opts) {
+		// Calling the super constructor is important so our generator is correctly set up
+		super(args, opts);
+	}
+	method() {
+		this.fs.copyTpl(
+			this.templatePath('index.html'),
+			this.destinationPath('public/index.html'),
+			{ title: 'Templating with Yeoman' }
+		);
+	}
+};
