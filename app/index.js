71f8e344c411e63d39fed34a1d447143f9ae43e7
Dinesh Dabhi <dinesh.dabhi@rapidops.com>
2017-09-26 03:21:42 +0530

    Changes for the basic index file to get the application name.

--- app/index.js
+++ app/index.js
@@ -1,16 +1,21 @@
-var Generator = require('yeoman-generator');
+let Generator = require('yeoman-generator'),
+	s = require('underscore.string');
 
 module.exports = class extends Generator {
 	// The name `constructor` is important here
 	constructor(args, opts) {
 		// Calling the super constructor is important so our generator is correctly set up
 		super(args, opts);
+		this.argument('appname', { type: String, required: false });
+		this.appname = this.options.appname || this.appname || path.basename(process.cwd());
+		this.appname = s.camelize(s.slugify(s.humanize(this.appname)));
+		console.log(this.appname);
 	}
-	method() {
+	createIndex() {
 		this.fs.copyTpl(
 			this.templatePath('index.html'),
-			this.destinationPath('public/index.html'),
-			{ title: 'Templating with Yeoman' }
+			this.destinationPath('index.html'),
+			{ title: this.appname }
 		);
 	}
 };
